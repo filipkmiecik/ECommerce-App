a4e1728fb6246a62da5e209d50de0c239f7bf7dd
Paaka <Mateuszhartabus@gmail.com>
2020-01-03 15:53:21 +0100

    Tes

--- ECommerce/Models/MockProductRepository.cs
+++ ECommerce/Models/MockProductRepository.cs
@@ -14,12 +14,12 @@ namespace ECommerce.Models
                 LoadProducts();
             }
         }
-
+        //Test
         private void LoadProducts()
         {
             products = new List<Product>
             {
-                new Product { Id = 1, Name = "Apple iPhone 8", Category = "Smartphones", Condition = "preowned", Description = "iPhone 8 bought January 2018 in perfect condition.", Price = 1900, Seller="shom123", ImageUrl = "/images/smartphone.jpg", ThumbnailUrl = "/images/smartphone.jpg", IsFeatured = false},
+                new Product { Id = 1, Name = "Apple IPhone 8", Category = "Smartphones", Condition = "preowned", Description = "iPhone 8 bought January 2018 in perfect condition.", Price = 1900, Seller="shom123", ImageUrl = "/images/smartphone.jpg", ThumbnailUrl = "/images/smartphone.jpg", IsFeatured = false},
                 new Product { Id = 2, Name = "Sony Speakers", Category = "Audio Devices", Condition = "new", Description = "5.1 Surround Home Theater Speakers.", Price = 4000, Seller="SonyPoland", ImageUrl = "/images/speakers.jpg", ThumbnailUrl = "/images/speakers.jpg", IsFeatured = false},
                 new Product { Id = 3, Name = "Nike Raygun ", Category = "Athletic Shoes", Condition = "preowned", Description = "Used Nike shoes, good condition.", Price =400, Seller="gransenio68", ImageUrl = "/images/shoes.jpg", ThumbnailUrl = "/images/shoes.jpg", IsFeatured = false},
                 new Product { Id = 4, Name = "Samsung Q70R 4K TV", Category = "Televisions", Condition = "new", Description = "TV with amazing picture quality and premium sound system.", Price = 3500, Seller="whover22", ImageUrl = "/images/tv.jpg", ThumbnailUrl = "/images/tv.jpg", IsFeatured = true},
